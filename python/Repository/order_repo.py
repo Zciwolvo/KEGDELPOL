fa13b4e18515b00e4f9611cdaa76231026e1ecdc
zciwolvo <zciwolvo@gmail.com>
2024-05-20 22:57:21 +0200

    authorization & driver updated

--- python/Repository/order_repo.py
+++ python/Repository/order_repo.py
@@ -1,11 +1,17 @@
+from models import Order
 from flask_sqlalchemy import SQLAlchemy
-from models import Order  # Assuming you have an Order model defined in models.py
-
 
 class OrderRepository:
-    def __init__(self, db):
-        self.db = db
+    def __init__(self):
+        self.db = SQLAlchemy()
 
-# 
-    def select_orders():
-        return self.db.session.query(Order).all()
+    def get_orders_for_driver(self, driver_username):
+        return Order.query.filter_by(driver_id=driver_username).all()
+
+    def update_order_status(self, order_id, driver_username, new_status):
+        order = Order.query.filter_by(order_id=order_id, driver_id=driver_username).first()
+        if order:
+            order.status = new_status
+            self.db.session.commit()
+            return True
+        return False
