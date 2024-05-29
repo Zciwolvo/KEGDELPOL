67df6889f979d90ed8789d927ffc9bc032840ebf
unknown <mirasowsky@gmail.com>
2024-05-29 20:23:13 +0200

    correct DropDowinInput Component

--- kegdelpol/src/components/DropDownInput.jsx
+++ kegdelpol/src/components/DropDownInput.jsx
@@ -1,44 +1,15 @@
 import React, { useState } from 'react';
 import './DropDownInput.css'; // Zaimportuj plik stylów CSS
 
-
 const DropdownInput = ({ label, options }) => {
   const [selectedOption, setSelectedOption] = useState('');
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
-    
-const DropDownInput = ({ label, options }) => {
-  const [inputValue, setInputValue] = useState('');
-  const [filteredOptions, setFilteredOptions] = useState([]);
-  const [isListVisible, setIsListVisible] = useState(false);
-
-  const handleInputChange = (event) => {
-    const value = event.target.value;
-    setInputValue(value);
-    filterOptions(value);
-    setIsListVisible(true);
-  };
-
-  const filterOptions = (value) => {
-    const filtered = options.filter(option =>
-      option.toLowerCase().includes(value.toLowerCase())
-    );
-    setFilteredOptions(filtered);
-  };
-
-  const handleOptionSelect = (value) => {
-    setInputValue(value);
-    setIsListVisible(false);
-    console.log(value);
-  };
-
-  const handleInputBlur = () => {
-    setIsListVisible(false);
   };
 
   return (
-    <div className="dropdown-input-container mb-3">
+    <div className="dropdown-input-container">
       <label>{label}</label>
       <select
         value={selectedOption}
@@ -50,29 +21,8 @@ const DropDownInput = ({ label, options }) => {
           <option key={index} value={option}>{option}</option>
         ))}
       </select>
-      <div className="dropdown-input">
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          onBlur={handleInputBlur}
-          placeholder="Type here..."
-          className="form-control"
-        />
-        {isListVisible && (
-          <div className="options-dropdown">
-            <ul className="options-list list-group">
-              {filteredOptions.map((option, index) => (
-                <li key={index} className="list-group-item" onMouseDown={() => handleOptionSelect(option)}>
-                  {option}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
     </div>
   );
 };
 
-export default DropDownInput;
+export default DropdownInput;
