fffdfb84204a0facfdc0e88f65e24ed10f7d811a
elpatrico11 <elpatrico21@gmail.com>
2024-06-01 19:03:39 +0200

    fixed employeeUi positioning

--- kegdelpol/src/pages/EmployeeUI.jsx
+++ kegdelpol/src/pages/EmployeeUI.jsx
@@ -2,24 +2,29 @@ import React from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import MoveTo from '../Components/MoveTo';
+import LogoutButton from '../Components/LogoutButton';
 import './EmployeeUI.css';
 
 const EmployeeUI = () => {
   return (
-    <div className="employee-ui-container">
+    <div className="employee-ui">
       <Navbar />
-
-      <div className="content">
-        <div className="choose-action">Choose your action</div>
-        <MoveTo imageSrc="/DriverLicense.svg" title="Assign The Driver" to="/AssignTheDriver" />
-        <MoveTo imageSrc="/AddUser.svg" title="Create New Account" to="/AddUser" />
-        <MoveTo imageSrc="/Database.svg" title="Database" to="/DataBase" />
-        <MoveTo imageSrc="/DeliveryBox.svg" title="Approve Order" to="/ApproveOrder" />
-        <MoveTo imageSrc="/UpdateUser.svg" title="Update The User" to="/UpdateUser" />
-        <MoveTo imageSrc="/Truck.svg" title="Add Vehicle" to="/AddVehicle" />
-      </div>
+            
+            <div className="grid-elements">
+              <div className="item-1">Choose your action</div>
+              <div className="item-2"><LogoutButton className="logout-button"/></div>
+              <div className="item-3"><MoveTo imageSrc="/DriverLicense.svg" title="Assign The Driver" to="/AssignTheDriverUI" /></div>
+              <div className="item-4"><MoveTo imageSrc="/AddUser.svg" title="Create New Account" to="/AddUser" /></div>
+              <div className="item-5"><MoveTo imageSrc="/Database.svg" title="Database" to="/DataBase" /></div>
+              <div className="item-6"><MoveTo imageSrc="/DeliveryBox.svg" title="Approve Order" to="/ApproveOrder" /></div>
+              <div className="item-7"><MoveTo imageSrc="/UpdateUser.svg" title="Update The User" to="/UpdateUser" /></div>
+              <div className="item-8"><MoveTo imageSrc="/Truck.svg" title="Add Vehicle" to="/AddVehicle" />  </div>
+              
+              
+            </div>
+          
       <Footer />
-    </div>
+      </div>
   );
 };
 
