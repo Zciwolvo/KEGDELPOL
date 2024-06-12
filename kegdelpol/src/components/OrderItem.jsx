e48931d6b982d07df3f668b6918eb24f78a308d7
unknown <mirasowsky@gmail.com>
2024-06-12 12:58:24 +0200

    update orders working, create new orders now send good variables to serwer

--- kegdelpol/src/components/OrderItem.jsx
+++ kegdelpol/src/components/OrderItem.jsx
@@ -21,6 +21,7 @@ const OrderItem = ({ order }) => {
       },
       body: JSON.stringify({ status: newStatus }),
     };
+    console.log(JSON.stringify({ status: newStatus }));
 
     fetch(`https://www.igorgawlowicz.pl/kegdelpol/order/orders/${order.order_id}`, requestOptions)
       .then((response) => {
