9ff6324cd7969ad290f1894c61993dad1459346c
elpatrico11 <elpatrico21@gmail.com>
2024-06-12 16:53:46 +0200

    fixed inputField

--- kegdelpol/src/components/InputField.jsx
+++ kegdelpol/src/components/InputField.jsx
@@ -1,12 +1,6 @@
-import React from 'react';
 import './InputField.css';
 
 const InputField = ({ label, id, type = "text", placeholder, value, onChange }) => {
-  const handleChange = (event) => {
-    const newValue = event.target.value;
-    onChange(newValue); // Call the onChange handler with the extracted value
-  };
-
   return (
     <div className="input-container">
       <label htmlFor={id} className="form-label">{label}</label>
@@ -16,7 +10,7 @@ const InputField = ({ label, id, type = "text", placeholder, value, onChange }) 
         placeholder={placeholder} 
         className="form-control input-field" 
         value={value}
-        onChange={handleChange} // Use the handleChange function
+        onChange={onChange}
       />
     </div>
   );
