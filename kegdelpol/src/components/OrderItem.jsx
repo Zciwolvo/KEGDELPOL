06550e84bd635f644c934d8668b0d1b53b9bf8ae
unknown <mirasowsky@gmail.com>
2024-06-12 12:23:46 +0200

    Update user working

--- kegdelpol/src/components/OrderItem.jsx
+++ kegdelpol/src/components/OrderItem.jsx
@@ -22,7 +22,7 @@ const OrderItem = ({ order }) => {
       body: JSON.stringify({ status: newStatus }),
     };
 
-    fetch(`https://www.igorgawlowicz.pl/kegdelpol/orders/${order.order_id}`, requestOptions)
+    fetch(`https://www.igorgawlowicz.pl/kegdelpol/order/orders/${order.order_id}`, requestOptions)
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
